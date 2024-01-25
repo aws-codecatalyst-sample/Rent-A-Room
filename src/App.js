bb4e1f60242f53790580f26e748ae6b3abb12eb6
yabzhang <103594938+aws-codecatalyst-sample@users.noreply.github.com>
2024-01-25 14:00:57 -0600

    Update App.js
    Add comments

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
